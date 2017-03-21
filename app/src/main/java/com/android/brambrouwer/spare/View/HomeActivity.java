92f13d5d303f68149339e5fc881be68c72f349e0
bbrouwer <b.brouwer@student.avans.nl>
2017-03-21 17:29:56 +0100

    Forced orientation

--- app/src/main/java/com/android/brambrouwer/spare/View/HomeActivity.java
+++ app/src/main/java/com/android/brambrouwer/spare/View/HomeActivity.java
@@ -2,6 +2,8 @@ package com.android.brambrouwer.spare.View;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -12,11 +14,21 @@ import android.widget.LinearLayout;
 import com.android.brambrouwer.spare.R;
 
 public class HomeActivity extends AppCompatActivity {
+    public String screenType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        screenType = getScreenType();
+
+        if(screenType.equals("normal")){
+            setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);     //  Fixed Portrait orientation
+        }else if(screenType.equals("large")){
+            setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);     //  Fixed Portrait orientation
+        }
+
         readPrefs();
     }
 
@@ -37,8 +49,6 @@ public class HomeActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-
-
     //Get current value of the theme pref & set backroung accordingly
     public void readPrefs(){
 
@@ -52,5 +62,26 @@ public class HomeActivity extends AppCompatActivity {
         }else{
             root.setBackgroundColor(Color.parseColor("#FFFFFF"));
         }
+    }
+
+    /*
+   Determine screentype. Returned as either normal or large
+    */
+    private String getScreenType(){
+
+        String screensize = "";
+
+        if ((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_LARGE ) {
+            screensize = "large";
+        }
+        else if ((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_NORMAL) {
+            screensize = "normal";
+        }
+        else if ((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_SMALL) {
+            screensize = "normal";
+        }else if ((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_XLARGE) {
+            screensize = "large";
+        }
+        return screensize;
     }
 }
